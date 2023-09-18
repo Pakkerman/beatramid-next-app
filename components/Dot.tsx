7dac3eb23b8e976cbe7c0f8697274964f7947479
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2023-09-18 20:54:09 +0800

    basic tick function

--- components/Dot.tsx
+++ components/Dot.tsx
@@ -1,23 +1,28 @@
 'use client'
 
+import { useControlContext } from '@/contexts/ControlContext'
 import React, { useEffect, useState } from 'react'
 
 let testing = false
 
-export default function Dot() {
+export default function Dot({ tick }: { tick: boolean }) {
+  const { BPM } = useControlContext()
   const [active, setActive] = useState<boolean>(false)
+  let lastTick = performance.now() - (60 * 1000) / BPM
 
   useEffect(() => {
-    if (!testing) return
-    const id = setInterval(() => {
-      setActive((prev) => !prev)
-    }, 1000)
-    return () => clearInterval(id)
-  }, [])
+    if (!tick) return
+
+    console.log(performance.now() - lastTick - (60 * 1000) / BPM)
+    setActive(true)
+    const id = setTimeout(() => setActive(false), 50)
+
+    // return () => clearTimeout(id)
+  }, [tick])
 
   return (
     <div
-      className={`w-4 h-4 rounded-full bg-slate-400 border transition-all ${
+      className={`w-4 h-4 rounded-full bg-slate-400 border ${
         active ? 'bg-opacity-100' : 'bg-opacity-0'
       }`}
     />
@@ -25,12 +30,13 @@ export default function Dot() {
 }
 
 export function DotRow({ count }: { count: number }) {
+  const { tick } = useControlContext()
   const array = new Array(count).fill(null)
   return (
     <ul className="flex flex-nowrap gap-3">
       {array.map((_, idx) => (
         <li key={idx}>
-          <Dot />
+          <Dot tick={tick} />
         </li>
       ))}
     </ul>
--- contexts/ControlContext.tsx
+++ contexts/ControlContext.tsx
@@ -4,6 +4,7 @@ import React, {
   SetStateAction,
   createContext,
   useContext,
+  useEffect,
   useState,
 } from 'react'
 
@@ -14,6 +15,8 @@ type ControlContext = {
   setVolume: React.Dispatch<React.SetStateAction<number>>
   BPM: number
   setBPM: React.Dispatch<React.SetStateAction<number>>
+  tick: boolean
+  setTick: React.Dispatch<React.SetStateAction<boolean>>
 }
 type ControlContextProviderProps = { children: React.ReactNode }
 
@@ -24,10 +27,34 @@ export function ControlContextProvider(props: ControlContextProviderProps) {
   const [beatActive, setBeatActive] = useState<boolean>(false)
   const [volume, setVolume] = useState<number>(0.6)
   const [BPM, setBPM] = useState<number>(85)
+  const [tick, setTick] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (!beatActive) return
+    const id = setInterval(() => {
+      console.log('tick on')
+      setTick(true)
+      setTimeout(() => {
+        console.log('tick off')
+        setTick(false)
+      }, 10)
+    }, (1000 * 60) / BPM)
+
+    return () => clearInterval(id)
+  }, [BPM, beatActive])
 
   return (
     <ControlContext.Provider
-      value={{ beatActive, setBeatActive, volume, setVolume, BPM, setBPM }}>
+      value={{
+        beatActive,
+        setBeatActive,
+        volume,
+        setVolume,
+        BPM,
+        setBPM,
+        tick,
+        setTick,
+      }}>
       {children}
     </ControlContext.Provider>
   )
